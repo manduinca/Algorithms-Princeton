998d46a320fb716ce937e267d17a68133220ea3c
manduinca <jeanpierre.mandujano@gmail.com>
2015-11-20 00:46:35 -0500

    Trabajo de semana 4 al 100% en Coursera

--- Week4/Board.java
+++ Week4/Board.java
@@ -2,17 +2,15 @@ import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.Queue;
 
 public class Board {
-    private int[] board;
-    private int[][] tempboard;
+    private int[][] board;
     private int N;
 
     public Board(int[][] blocks) {
         N = blocks[0].length;
-        board = new int[N * N];
-        tempboard = new int[N][N];
+        board = new int[N][N];
         for(int i = 0; i < N; i++)
             for(int j = 0; j < N; j++)
-                board[i * N + j] = blocks[i][j];
+                board[i][j] = blocks[i][j];
     }
     private int goal(int i, int j) {
         if(i * N + j < N * N -1)
@@ -27,7 +25,7 @@ public class Board {
         for(int i = 0; i < N; i++)
             for(int j = 0; j < N; j++)
                 if(goal(i, j) != 0)
-                    if(goal(i, j) != board[i * N + j])
+                    if(goal(i, j) != board[i][j])
                         out++;
         return out;
     }
@@ -39,36 +37,35 @@ public class Board {
         int sum = 0;
         for(int i = 0; i < N; i++)
             for(int j = 0; j < N; j++)
-                if(board[i * N + j] != 0)
-                    sum += abs(i, (board[i * N + j] - 1) / N) +
-                           abs(j, board[i * N + j] - (board[i * N + j] - 1) / N * N - 1);
+                if(board[i][j] != 0)
+                    sum += abs(i, (board[i][j] - 1) / N) +
+                           abs(j, board[i][j] - (board[i][j] - 1) / N * N - 1);
         return sum;
     }
     public boolean isGoal() {
         for(int i = 0; i < N; i++)
             for(int j = 0; j < N; j++)
-                if(goal(i, j) != board[i * N + j]) return false;
+                if(goal(i, j) != board[i][j]) return false;
         return true;
     }
     public Board twin() {
-        int[][] boardtwin = new int[N][N];
-        for(int i = 0; i < N; i++)
-            for(int j = 0; j < N; j++)
-                boardtwin[i][j] = board[i * N + j];
         int ri = StdRandom.uniform(N), rj = StdRandom.uniform(N);
-        while(boardtwin[ri][rj] == 0) {
+        while(board[ri][rj] == 0) {
             ri = StdRandom.uniform(N);
             rj = StdRandom.uniform(N);
         }
         int rx = StdRandom.uniform(N), ry = StdRandom.uniform(N);
-        while(boardtwin[rx][ry] == 0 || (rx == ri && ry == rj)) {
+        while(board[rx][ry] == 0 || (rx == ri && ry == rj)) {
             rx = StdRandom.uniform(N);
             ry = StdRandom.uniform(N);
         }
-        int aux = boardtwin[ri][rj];
-        boardtwin[ri][rj] = boardtwin[rx][ry];
-        boardtwin[rx][ry] = aux;
-        Board btwin = new Board(boardtwin);
+        int aux = board[ri][rj];
+        board[ri][rj] = board[rx][ry];
+        board[rx][ry] = aux;
+        Board btwin = new Board(board);
+        aux = board[ri][rj];
+        board[ri][rj] = board[rx][ry];
+        board[rx][ry] = aux;
         return btwin;
     }
     public boolean equals(Object x) {
@@ -79,13 +76,8 @@ public class Board {
         if(N != y.N) return false;
         for(int i = 0; i < N; i++)
             for(int j = 0; j < N; j++)
-                if(board[i * N + j] != y.board[i * N + j]) return false;
+                if(board[i][j] != y.board[i][j]) return false;
         return true;
-    }
-    private void copyBoard() {
-        for(int i = 0; i < N; i++)
-            for(int j = 0; j < N; j++)
-                tempboard[i][j] = board[i * N + j];
     }
     public Iterable <Board> neighbors() {
         int ii = 0, jj = 0;
@@ -93,32 +85,36 @@ public class Board {
         Queue <Board> pila = new Queue();
         for(int i = 0; i < N; i++) {
             for(int j = 0; j < N; j++)
-                if(board[i * N + j] == 0) {ii = i; jj = j; band = true; break;}
+                if(board[i][j] == 0) {ii = i; jj = j; band = true; break;}
             if(band) break;
         }
         if(ii - 1 >= 0) {
-            copyBoard();
-            tempboard[ii][jj] = tempboard[ii - 1][jj];
-            tempboard[ii - 1][jj] = 0; 
-            pila.enqueue(new Board(tempboard));
+            board[ii][jj] = board[ii - 1][jj];
+            board[ii - 1][jj] = 0;
+            pila.enqueue(new Board(board));
+            board[ii - 1][jj] = board[ii][jj];
+            board[ii][jj] = 0;
         }
         if(jj - 1 >= 0) {
-            copyBoard();
-            tempboard[ii][jj] = tempboard[ii][jj - 1];
-            tempboard[ii][jj - 1] = 0; 
-            pila.enqueue(new Board(tempboard));
+            board[ii][jj] = board[ii][jj - 1];
+            board[ii][jj - 1] = 0; 
+            pila.enqueue(new Board(board));
+            board[ii][jj - 1] = board[ii][jj];
+            board[ii][jj] = 0; 
         }
         if(ii + 1 < N) {
-            copyBoard();
-            tempboard[ii][jj] = tempboard[ii + 1][jj];
-            tempboard[ii + 1][jj] = 0; 
-            pila.enqueue(new Board(tempboard));
+            board[ii][jj] = board[ii + 1][jj];
+            board[ii + 1][jj] = 0; 
+            pila.enqueue(new Board(board));
+            board[ii + 1][jj] = board[ii][jj];
+            board[ii][jj] = 0; 
         }
         if(jj + 1 < N) {
-            copyBoard();
-            tempboard[ii][jj] = tempboard[ii][jj + 1];
-            tempboard[ii][jj + 1] = 0; 
-            pila.enqueue(new Board(tempboard));
+            board[ii][jj] = board[ii][jj + 1];
+            board[ii][jj + 1] = 0; 
+            pila.enqueue(new Board(board));
+            board[ii][jj + 1] = board[ii][jj];
+            board[ii][jj] = 0; 
         }
         return pila;
     }
@@ -127,8 +123,8 @@ public class Board {
         s.append(N + "\n");
         for(int i = 0; i < N; i++) {
             for(int j = 0; j < N - 1; j++)
-                s.append(board[i * N + j] + " ");
-            s.append(board[i * N + N - 1] + "\n");
+                s.append(board[i][j] + " ");
+            s.append(board[i][N - 1] + "\n");
         }
         return s.toString(); 
     }
